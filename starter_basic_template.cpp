4f38fbc9d8e0580331471114a75f11754380228c
Vaibhav Malaviya <55515799+vaimalaviya1233@users.noreply.github.com>
2023-02-06 17:32:25 -0600

    Create starter_basic_template.cpp

---
+++ starter_basic_template.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // imports input/output stream library
+using namespace std; // uses standard namespace of C++ 
+
+int main(){// default main class 
+  
+  cout << "Hello World" << endl; // "cout" = console out (prints in console keyword) :: "<<" is stream insertion operator :: "endl" is to indicate to program to end line. 
+  return 0;// tells system program is ended by returning zero.
+} 
